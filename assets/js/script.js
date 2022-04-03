1c0d735d58c69e1992dc3a6a7e35c39f3b77f4cf
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-04-03 14:36:11 +0930

    Added variables, array of quizQuestions, and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,55 @@
-var quiz = document.getElementById("quiz");
+var quiz = document.querySelector(".quiz");
 var startBtn = document.getElementById("start");
+var timerElement = document.querySelector(".quiz-timer");
+var savedResults = document.querySelector(".results");
+var quizQuestions = document.getElementById("quiz-questions")
+
+var quizScore = 0;
+var secondsLeft = 60;
+
+// Array of questions that will be asked and the correct answers
+var quizQuestions = [
+    {
+        question: "What of the following is NOT a common data type?",
+        answers: {
+          a: "Boolean",
+          b: "Number",
+          c: "Justification",
+          d: "String"  ,
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Arrays can be used to store:",
+        answers: {
+            a: "Strings and numbers",
+            b: "Other arrays",
+            c: "Booleans",
+            d: "All of the above"
+        },
+        correctAnswer: "d"
+    }
+];
 
 // Testing start button functionality
-function jsQuiz() {
+function generateQuiz() {
+    startTimer();
     console.log("This works!")
 }
 
-startBtn.addEventListener("click", jsQuiz);
+// When start button is pressed, the timer begins and starts to count down
+function startTimer() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerElement.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            console.log("Times up")
+        }
+    }, 1000);
+}
+
+
+
+startBtn.addEventListener("click", generateQuiz);
