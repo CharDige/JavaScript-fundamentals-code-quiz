21430fcb89ad20cf507b09dd30611e63d3340074
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-04-03 13:10:34 +0930

    Testing jsQuiz function with console log

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var quiz = document.getElementById("quiz");
+var startBtn = document.getElementById("start");
+
+// Testing start button functionality
+function jsQuiz() {
+    console.log("This works!")
+}
+
+startBtn.addEventListener("click", jsQuiz);
